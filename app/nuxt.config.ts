abc9db7760ee95c81f066a0bf8184280f49ab6cf
Tom Härter <tom@dokedu.org>
2023-03-26 17:44:59 +0700

    add tailwind css

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    telemetry: false,
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
---
+++ app/pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input type="text" v-model="username" placeholder="Username">
+            <input type="password" v-model="password" placeholder="Password">
+            <button type="submit" class="bg-blue-500">Login</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+function login() {
+    console.log(username.value, password.value)
+}
+</script>
